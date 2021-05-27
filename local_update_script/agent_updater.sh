3cb58ada2dec879b40988ba4bc49eaa185cf3dc5
GMastaP <gmastap@gmail.com>
2021-05-27 17:21:00 +0000

    adding agent update script for client

---
+++ local_update_script/agent_updater.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# local script to update the  checkmk_agent based on https://<SERVER>/<INSTANCE>_latest_deb_agent
+#
+#
+
+# Variables
+DLAGENT="" #download string for the agent
+AGENT="checkmk_agent.deb" #
+LINUX=$(cat /etc/issue | awk '{ print $1 }')
+INSTANCE="INSTANCE"
+DLSERVER="192.168.0.100"
+
+# for Ubunut Systems
+if [ $LINUX == "Ubuntu" ] || [ OR $LINUX == "Debian" ]
+ then
+    DLAGENT=$INSTANCE"_latest_deb_agent"
+
+fi
+
+curl -s -k https://$DLSERVER/$DLAGENT -o $AGENT
+
+INSTALLEDAGENT=$(sudo apt-cache policy check-mk-agent | grep Installed | awk '{ print $2 }')
+REMOTEAGENT=$(dpkg-deb -f ./$AGENT Version )
+
+
+echo $INSTALLEDAGENT" vs. "$REMOTEAGENT
+
+if [ "$INSTALLEDAGENT" !=  "$REMOTEAGENT" ]
+ then
+    echo "unequal"
+    sudo apt install ./$AGENT -y
+fi
